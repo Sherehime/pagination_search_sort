43cfb965246bd1d358faf6094a1cd8bb470fc2e1
Alex Owl <64910746+Sherehime@users.noreply.github.com>
2021-03-19 14:29:26 +0300

    Initial commit

---
+++ src/container.js
@@ -0,0 +1,102 @@
+export const allData = [
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale2",
+    parentId: "123"
+  },
+  {
+    name: "Sale34",
+    parentId: "45"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  },
+  {
+    name: "Sale",
+    parentId: "12"
+  }
+];
